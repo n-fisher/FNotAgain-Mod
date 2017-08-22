b9f3fd4861d1fb763354fe785ccd6b77be2e4414
Nick <nick0fisher@gmail.com>
2017-08-22 13:30:10 -0700

    - cleaned up code a bit - updated descriptions - removed languages folder for now

--- Source/FNotAgain_Mod.cs
+++ Source/FNotAgain_Mod.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using HugsLib;
 using System.Collections.Generic;
 using Verse;
 using HugsLib.Settings;
-using HugsLib.Source.Settings;
-using System.Xml.Serialization;
 using Harmony;
 using RimWorld;
-using RimWorld.Planet;
-using System.Reflection;
 using System.Linq;
-using Newtonsoft.Json;
 
 namespace FNotAgain_Mod
 {
@@ -25,7 +19,6 @@ namespace FNotAgain_Mod
         public override void Initialize()
         {
             Crashland.settings = this.Settings;
-            SaveSurvivors.settings = this.Settings;
             base.Initialize();
         }
 
@@ -43,7 +36,6 @@ namespace FNotAgain_Mod
         }
 
         public const string Identifier = "FNotAgain_Mod";
-        public const string HarmonyInstanceId = "FNotAgain_Mod";
 
         private SettingHandle<bool> isCrashlanding;
 
@@ -68,7 +60,7 @@ namespace FNotAgain_Mod
 
         public override void DefsLoaded()
         {
-            isCrashlanding = Settings.GetHandle<bool>("isCrashlanding", "toggleSetting_label".Translate(), "toggleSetting_desc".Translate(), true);
+            isCrashlanding = Settings.GetHandle<bool>("isCrashlanding", "Crashlanding?", "Auto-load saved colonists in new colony creation.", true);
         }
         
         [HarmonyPatch(typeof(Page_ConfigureStartingPawns), "PreOpen")]
@@ -79,17 +71,13 @@ namespace FNotAgain_Mod
             [HarmonyPrefix]
             public static void Crashland_Main()
             {
-                FNotAgain_Mod.Instance.Logger.Message("getting saved pawns");
                 var pawns = FNotAgain_Mod.Instance.SavedPawns;
-                FNotAgain_Mod.Instance.Logger.Message("checking for crash");
                 if (settings.GetHandle<bool>("isCrashlanding") && pawns.Count != 0)
                 {
                     foreach(Pawn p in pawns)
                     {
-                        FNotAgain_Mod.Instance.Logger.Message("Clearing relations");
                         p.relations.ClearAllRelations();
                     }
-                    FNotAgain_Mod.Instance.Logger.Message("Loaded " + pawns.Count + " pawns");
                     Current.Game.InitData.startingPawns = pawns;
                 }
             }
@@ -98,21 +86,16 @@ namespace FNotAgain_Mod
         [HarmonyPatch(typeof(ShipCountdown), "InitiateCountdown")]
         class SaveSurvivors
         {
-            public static ModSettingsPack settings;
-
             [HarmonyPrefix]
             public static void InitiateCountdown_Patch(ref Building launchingShipRoot, ref int journeyDestinationTile)
             {
                 List<Pawn> pawnsToSave = new List<Pawn>();
-                FNotAgain_Mod.Instance.Logger.Message("else");
                 List<Building> list = ShipUtility.ShipBuildingsAttachedTo(launchingShipRoot).ToList<Building>();
                 foreach (Building current in list)
                 {
-                    FNotAgain_Mod.Instance.Logger.Message("foreach");
                     Building_CryptosleepCasket building_CryptosleepCasket = current as Building_CryptosleepCasket;
                     if (building_CryptosleepCasket != null && building_CryptosleepCasket.HasAnyContents && building_CryptosleepCasket.ContainedThing.GetType() == typeof(Pawn))
                     {
-                        FNotAgain_Mod.Instance.Logger.Message("woo");
                         pawnsToSave.Add((Pawn)building_CryptosleepCasket.ContainedThing);
                     }
                 }
@@ -120,9 +103,5 @@ namespace FNotAgain_Mod
                 FNotAgain_Mod.Instance.Logger.Message("Saved " + FNotAgain_Mod.Instance.SavedPawns.Count + " pawns");
             }
         }
-        
-        [Serializable]
-        class SerializablePawn : Pawn
-        {}
     }
 }
